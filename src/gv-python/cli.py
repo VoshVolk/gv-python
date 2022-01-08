d259e7b0f778940ef53daeb52e55c614224e2474
VoshVolk <hinosato01@gmail.com>
2022-01-08 14:38:44 +0900

    main

---
+++ src/gv-python/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def ocr():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "source",
+        type=str,
+        help="This is pdf source file or dir."
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="Give more output."
+    )
